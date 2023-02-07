c635f4a46b69feda090c6fb5d3b42763c17bc422
Nicholas Chong <chongyy03@gmail.com>
2023-02-07 10:50:53 +0800

    updated

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -9,6 +9,10 @@ from django.contrib.auth import get_user_model
 
 from core import models
 
+def create_user(email="user@example.com", password="testpass123"):
+    """"Create and return a new user"""
+    return get_user_model().objects.create_user(email,password)
+
 class ModelTests(TestCase):
     """Test models"""
 
@@ -51,17 +55,24 @@ class ModelTests(TestCase):
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
 
-def test_create_recipe(self):
-    """Test creating a recipe is successful"""
-    user = get_user_model().objects.create_user(
-        "test@example.com",
-        "testpass123",
-    )
-    recipe = models.Recipe.objects.create(
-        user = user,
-        title = "Sample recipe name",
-        price = Decimal("5.50"),
-        description = "Sample recipe description.",
-    )
+    def test_create_recipe(self):
+        """Test creating a recipe is successful"""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "testpass123",
+        )
+        recipe = models.Recipe.objects.create(
+            user = user,
+            title = "Sample recipe name",
+            price = Decimal("5.50"),
+            description = "Sample recipe description.",
+        )
 
-    self.assertEqual(str(recipe), recipe.title)
+        self.assertEqual(str(recipe), recipe.title)
+
+    def test_create_tag(self):
+        """Test creating a tag is successful"""
+        user = create_user()
+        tag = models.Tag.objects.create(user=user, name="Tag1")
+
+        self.assertEqual(str(tag), tag.name)
